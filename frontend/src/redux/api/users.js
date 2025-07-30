12cea9f17d1b7bae52ee56f5d0c93d11a3beeb62
ARYAN242526 <roy2503aryan@gmail.com>
2025-07-30 13:28:07 +0530

    login and logout functionality done

--- frontend/src/redux/api/users.js
+++ frontend/src/redux/api/users.js
@@ -11,15 +11,21 @@ export const userApiSlice = apiSlice.injectEndpoints({
                 method : 'POST',
                 body : data,
             })
-        }) , 
+        }), 
         register : builder.mutation({
             query : (data) => ({
                 url : `${USERS_URL}`,
                 method : "POST",
                 body : data
             })
+        }),
+        logout : builder.mutation({
+            query : (data) => ({
+                url : `${USERS_URL}/logout`,
+                method : "POST",
+            })
         })
     })
 })
 
-export const {useLoginMutation , useRegisterMutation} = userApiSlice;
+export const {useLoginMutation , useRegisterMutation , useLogoutMutation} = userApiSlice;
